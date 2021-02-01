aadf78c4f5bab4e0a00ea78a0249fcce1e740e89
Tobias Mock <tobiasjammer@googlemail.com>
2021-02-01 16:19:52 +0100

    Static linking on windows

---
+++ src/config/configure.ml
@@ -0,0 +1,11 @@
+module C = Configurator.V1
+
+let () =
+  C.main ~name:"raylib" (fun c ->
+      let link_flags =
+        match C.ocaml_config_var c "system" with
+        | Some "mingw64" -> ["-cclib"; "-Wl,-static"]
+        | Some _ | None -> []
+      in
+
+      C.Flags.write_sexp "link_flags.sexp" link_flags)
