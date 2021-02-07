232d5a0c2bd12b4c2dc5bc81c3ad09947a304e4f
Tobias Jammer <tobiasjammer@googlemail.com>
2021-02-07 13:21:25 +0100

    Don't open console on windows

--- src/config/configure.ml
+++ src/config/configure.ml
@@ -4,7 +4,15 @@ let () =
   C.main ~name:"raylib" (fun c ->
       let link_flags =
         match C.ocaml_config_var c "system" with
-        | Some "mingw64" -> [ "-cclib"; "-Wl,-static" ]
+        | Some "mingw64" ->
+            [
+              "-cclib";
+              "-Wl,-static";
+              "-cclib";
+              "-subsystem";
+              "-cclib";
+              "windows";
+            ]
         | Some _ | None -> []
       in
 
