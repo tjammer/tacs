8a3a8747cdbfaf94971b0ef6260d218046b43094
Tobias Mock <tobiasjammer@googlemail.com>
2021-02-11 18:43:58 +0100

    Add Singleplayer to menu

--- src/tacs.ml
+++ src/tacs.ml
@@ -5,7 +5,6 @@ let width = 1280
 
 let height = 720
 
-(* type button = {} *)
 type bar = {
   width : int;
   border : int;
@@ -20,9 +19,15 @@ type buttonstate =
   | Anim_idle of bar * bar * float
   | Hover of bar
 
-type mode = Sp | Mp
+type mode = Local | Mp | Sp
 
 type button = { bar : bar; x : int; y : int; state : buttonstate; mode : mode }
+
+let button_xy { bar; x; y; state; mode = _ } =
+  match state with
+  | Idle _ -> (x, y)
+  | Anim_hover (src, _, _) | Anim_idle (src, _, _) | Hover src ->
+      (x - ((bar.width - src.width) / 2), y - ((bar.height - src.height) / 2))
 
 let draw_bar b x y =
   (* draw outer border *)
@@ -87,21 +92,35 @@ let animate_button a b t =
     innerc = b.innerc;
   }
 
+let layout_buttons bar lst x y w h =
+  (* only vertically, take first bar as prototype *)
+  let len = List.length lst in
+  let xofs = (w - bar.width) / 2 in
+  (* try to have same space above, below and between *)
+  let vspace = (h - (len * bar.height)) / (len + 1) in
+  List.mapi
+    ~f:(fun i (state, mode) ->
+      {
+        bar;
+        x = x + xofs;
+        y = y + (((i + 1) * vspace) + (i * bar.height));
+        state = Idle state;
+        mode;
+      })
+    lst
+
 let setup_window () =
   let open Raylib in
   set_config_flags [ ConfigFlag.VSync_hint ];
   init_window 1280 720 "tacs";
   set_exit_key Key.F12;
-  [
-    { bar; x = 150; y = 375; state = Idle hover_bar; mode = Sp };
-    {
-      bar;
-      x = width - bar.width - 150;
-      y = 375;
-      state = Idle { hover_bar with innerc = Color.orange };
-      mode = Mp;
-    };
-  ]
+  layout_buttons bar
+    [
+      (hover_bar, Sp);
+      ({ hover_bar with innerc = Color.orange }, Local);
+      ({ hover_bar with innerc = Color.maroon }, Mp);
+    ]
+    80 0 (width / 2) height
 
 let rec loop control buttons =
   let open Raylib in
@@ -113,30 +132,35 @@ let rec loop control buttons =
       let mx, my = (get_mouse_x (), get_mouse_y ()) in
       let buttons =
         List.map
-          ~f:(fun ({ bar; x; y; state; mode = _ } as br) ->
+          ~f:(fun but ->
+            let x, y = button_xy but in
             let hover =
-              mx > x && mx < x + bar.width && my > y && my < y + bar.height
+              mx > x
+              && mx < x + but.bar.width
+              && my > y
+              && my < y + but.bar.height
             in
-            match (state, hover) with
-            | Idle dst, true -> { br with state = Anim_hover (bar, dst, 0.0) }
-            | Idle _, false -> br
+            match (but.state, hover) with
+            | Idle dst, true ->
+                { but with state = Anim_hover (but.bar, dst, 0.0) }
+            | Idle _, false -> but
             | Anim_hover (src, dst, frac), true ->
                 let frac = frac +. (1.0 /. 15.0) in
-                if frac >=. 1.0 then { br with bar = dst; state = Hover src }
+                if frac >=. 1.0 then { but with bar = dst; state = Hover src }
                 else
                   {
-                    br with
+                    but with
                     bar = animate_button src dst frac;
                     state = Anim_hover (src, dst, frac);
                   }
             | Anim_hover (src, dst, frac), false ->
-                { br with state = Anim_idle (src, dst, frac) }
+                { but with state = Anim_idle (src, dst, frac) }
             | Anim_idle (src, dst, frac), false ->
                 let frac = frac -. (1.0 /. 15.0) in
-                if frac <=. 0.0 then { br with bar = src; state = Idle dst }
+                if frac <=. 0.0 then { but with bar = src; state = Idle dst }
                 else
                   {
-                    br with
+                    but with
                     bar =
                       {
                         (animate_button src dst frac) with
@@ -145,9 +169,10 @@ let rec loop control buttons =
                     state = Anim_idle (src, dst, frac);
                   }
             | Anim_idle (src, dst, frac), true ->
-                { br with state = Anim_hover (src, dst, frac) }
-            | Hover _, true -> br
-            | Hover src, false -> { br with state = Anim_idle (src, bar, 1.0) })
+                { but with state = Anim_hover (src, dst, frac) }
+            | Hover _, true -> but
+            | Hover src, false ->
+                { but with state = Anim_idle (src, but.bar, 1.0) })
           buttons
       in
 
@@ -155,10 +180,13 @@ let rec loop control buttons =
         if is_mouse_button_pressed MouseButton.Left then
           List.fold_left
             ~f:(fun res bt ->
-              match (bt.state, bt.mode) with
-              | Idle _, _ | Anim_idle _, _ -> res
-              | Hover _, Sp | Anim_hover _, Sp -> Some Local.start
-              | Hover _, Mp | Anim_hover _, Mp -> Some Multiplayer.connect)
+              match bt.state with
+              | Idle _ | Anim_idle _ -> res
+              | Hover _ | Anim_hover _ -> (
+                  match bt.mode with
+                  | Sp -> Some Solo.start
+                  | Local -> Some Local.start
+                  | Mp -> Some Multiplayer.connect ))
             ~init:None buttons
         else None
       with
@@ -171,24 +199,21 @@ let rec loop control buttons =
           clear_background Color.raywhite;
 
           List.iter
-            ~f:(fun { bar; x; y; state; mode } ->
-              let x, y =
-                match state with
-                | Idle _ -> (x, y)
-                | Anim_hover (src, _, _) | Anim_idle (src, _, _) | Hover src ->
-                    ( x - ((bar.width - src.width) / 2),
-                      y - ((bar.height - src.height) / 2) )
-              in
-              draw_bar_text bar x y
-                (match mode with Sp -> "Local" | Mp -> "Multiplayer")
+            ~f:(fun but ->
+              let x, y = button_xy but in
+              draw_bar_text but.bar x y
+                ( match but.mode with
+                | Local -> "Local"
+                | Mp -> "Multiplayer"
+                | Sp -> "Singleplayer" )
                 50)
             buttons;
 
           let txt = "TACS" in
           let sz = 100 in
           let w = measure_text txt sz in
-          let x = (width / 2) - (w / 2) in
-          draw_text txt x 100 sz Color.gray;
+          let x = (width * 2 / 3) - (w / 6) in
+          draw_text txt x 175 sz Color.gray;
 
           end_drawing ();
           if is_key_pressed Key.Escape then loop `Exit buttons
