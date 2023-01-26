1442695245dba8f1d9ef0f140d4428c49761804f
Tobias Mock <mail@tmock.de>
2023-01-26 21:46:14 +0100

    Use csexp instead of sexplib
    Fix parsing

--- src/msg/msg.ml
+++ src/msg/msg.ml
@@ -1,3 +1,4 @@
+module Sexp = Csexp.Make (Sexplib0.Sexp)
 open Sexplib0.Sexp_conv
 
 type start = { starting : Game.Team.t; move_seed : int } [@@deriving sexp]
@@ -5,14 +6,10 @@ type start = { starting : Game.Team.t; move_seed : int } [@@deriving sexp]
 type t = Search | Found of Game.Team.t | Start of start | Move of Game.Input.t
 [@@deriving sexp]
 
-let string_of_t v = Sexplib0.Sexp.to_string_mach @@ sexp_of_t v
+let string_of_t v = Sexp.to_string @@ sexp_of_t v
 
 let parse msg =
-  print_endline @@ "trying to parse : ." ^ msg ^ ".";
-  match Sexplib.Sexp.of_string msg with
-  | sexp -> (
-      match t_of_sexp sexp with
-      | msg -> Some msg
-      | exception Sexplib.Conv.Of_sexp_error (_, _) -> None)
-  (* is this wise? *)
-  | exception _ -> None
+  print_endline @@ "trying to parse : \"" ^ msg ^ "\"";
+  match Sexp.parse_string msg with
+  | Ok sexp -> Some (t_of_sexp sexp)
+  | Error _ -> None
